785b22105a31ba74b095be01c7f738240fa357de
AvishekPD <57665042+AvishekPD@users.noreply.github.com>
2020-12-02 11:29:12 +0530

    new color scheme

--- config.h
+++ config.h
@@ -101,31 +101,31 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"cyan",
-	"#5c5cff",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000",
+	"#393939",
+	"#393939",
+	"#222222",
+	"#333333",
+	"#444444",
+	"#555555",
+	"#777777",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"cyan",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#ffffff",
+	"#090909",
+	"#181818",
+	"#272727",
+	"#393939",
+	"#484848",
+	"#595959",
+	"#818181",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	  "#cccccc",
+	 "#555555",
+	 "black",  
 };
 
 
