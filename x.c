3005529447255eae3fb8b2afdb225d25a8cf5d5e
AvishekPD <57665042+AvishekPD@users.noreply.github.com>
2021-08-31 13:09:24 +0530

    Added support for ligatures

--- x.c
+++ x.c
@@ -19,6 +19,7 @@ char *argv0;
 #include "arg.h"
 #include "st.h"
 #include "win.h"
+#include "hb.h"
 
 /* types used in config.h */
 typedef struct {
@@ -1209,6 +1210,9 @@ xunloadfont(Font *f)
 void
 xunloadfonts(void)
 {
+    /* Clear Harbuzz buffer */
+    hbunloadfonts();
+
 	/* Free the loaded fonts in the font cache.  */
 	while (frclen > 0)
 		XftFontClose(xw.dpy, frc[--frclen].font);
@@ -1419,7 +1423,7 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 		mode = glyphs[i].mode;
 
 		/* Skip dummy wide-character spacing. */
-		if (mode == ATTR_WDUMMY)
+		if (mode & ATTR_WDUMMY)
 			continue;
 
 		/* Determine font for glyph if different from previous glyph. */
@@ -1525,6 +1529,8 @@ xmakeglyphfontspecs(XftGlyphFontSpec *specs, const Glyph *glyphs, int len, int x
 		xp += runewidth;
 		numspecs++;
 	}
+    /* Harbuzz transformation for ligatures */ 
+    hbtransform(specs, glyphs, len, x, y);
 
 	return numspecs;
 }
@@ -1675,14 +1681,14 @@ xdrawglyph(Glyph g, int x, int y)
 }
 
 void
-xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
+xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og, Line line, int len)
 {
 	Color drawcol;
 
 	/* remove the old cursor */
 	if (selected(ox, oy))
 		og.mode ^= ATTR_REVERSE;
-	xdrawglyph(og, ox, oy);
+	xdrawline(line, 0, oy, len); //Fix the broken ligatures
 
 	if (IS_SET(MODE_HIDE))
 		return;
